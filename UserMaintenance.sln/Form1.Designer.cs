3a4096100e1a8b384f9899345eeb0df34f7544a2
vidorgyozo <gyozovidor@gmail.com>
2020-12-02 18:06:41 +0100

    User list, and Add User function.

--- UserMaintenance.sln/Form1.Designer.cs
+++ UserMaintenance.sln/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.usersListBox = new System.Windows.Forms.ListBox();
+            this.lNameLabel = new System.Windows.Forms.Label();
+            this.fNameLabel = new System.Windows.Forms.Label();
+            this.lNameTextBox = new System.Windows.Forms.TextBox();
+            this.fNameTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usersListBox
+            // 
+            this.usersListBox.FormattingEnabled = true;
+            this.usersListBox.Location = new System.Drawing.Point(13, 13);
+            this.usersListBox.Name = "usersListBox";
+            this.usersListBox.Size = new System.Drawing.Size(120, 95);
+            this.usersListBox.TabIndex = 0;
+            // 
+            // lNameLabel
+            // 
+            this.lNameLabel.AutoSize = true;
+            this.lNameLabel.Location = new System.Drawing.Point(140, 16);
+            this.lNameLabel.Name = "lNameLabel";
+            this.lNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.lNameLabel.TabIndex = 1;
+            this.lNameLabel.Text = "label1";
+            // 
+            // fNameLabel
+            // 
+            this.fNameLabel.AutoSize = true;
+            this.fNameLabel.Location = new System.Drawing.Point(140, 43);
+            this.fNameLabel.Name = "fNameLabel";
+            this.fNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.fNameLabel.TabIndex = 2;
+            this.fNameLabel.Text = "label2";
+            // 
+            // lNameTextBox
+            // 
+            this.lNameTextBox.Location = new System.Drawing.Point(212, 13);
+            this.lNameTextBox.Name = "lNameTextBox";
+            this.lNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lNameTextBox.TabIndex = 3;
+            // 
+            // fNameTextBox
+            // 
+            this.fNameTextBox.Location = new System.Drawing.Point(212, 40);
+            this.fNameTextBox.Name = "fNameTextBox";
+            this.fNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fNameTextBox.TabIndex = 4;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(176, 84);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "button1";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.fNameTextBox);
+            this.Controls.Add(this.lNameTextBox);
+            this.Controls.Add(this.fNameLabel);
+            this.Controls.Add(this.lNameLabel);
+            this.Controls.Add(this.usersListBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox usersListBox;
+        private System.Windows.Forms.Label lNameLabel;
+        private System.Windows.Forms.Label fNameLabel;
+        private System.Windows.Forms.TextBox lNameTextBox;
+        private System.Windows.Forms.TextBox fNameTextBox;
+        private System.Windows.Forms.Button addButton;
     }
 }
 
--- UserMaintenance.sln/Form1.cs
+++ UserMaintenance.sln/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.sln.Entities;
 
 namespace UserMaintenance.sln
 {
     public partial class Form1 : Form
     {
+        private BindingList<User> _users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
+
+            lNameLabel.Text = Resource1.LastName;
+            fNameLabel.Text = Resource1.FirstName;
+            addButton.Text = Resource1.Add;
+
+            usersListBox.DataSource = _users;
+            usersListBox.ValueMember = "ID";
+            usersListBox.DisplayMember = "FullName";
+
+            addButton.Click += AddButton_Click;
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            User u = new User();
+            u.LastName = lNameTextBox.Text;
+            u.FirstName = fNameTextBox.Text;
+            _users.Add(u);
         }
     }
 }
