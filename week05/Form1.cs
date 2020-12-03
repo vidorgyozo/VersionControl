40f5ddd99b0c26ef48714a890ec93358efc32ce4
vidorgyozo <gyozovidor@gmail.com>
2020-12-03 14:00:30 +0100

    Task 5 in progress.

--- week05/Form1.cs
+++ week05/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.Entities;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
+        List<PortfolioItem> Portfolio;
+
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            throw new NotImplementedException();
         }
     }
 }
