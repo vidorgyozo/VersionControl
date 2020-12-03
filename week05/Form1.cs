aa54b6aa258fe6de317133e0fa1f08ece9c63c84
vidorgyozo <gyozovidor@gmail.com>
2020-12-03 14:28:05 +0100

    Task 7 done.

--- week05/Form1.cs
+++ week05/Form1.cs
@@ -36,5 +36,20 @@ namespace week05
 
             dataGridView2.DataSource = Portfolio;
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
