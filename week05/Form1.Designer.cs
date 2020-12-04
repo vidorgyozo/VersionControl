562b1b48b9b84d27ab47d23463e07f3d25ad2b56
vidorgyozo <gyozovidor@gmail.com>
2020-12-04 23:54:41 +0100

    Profit list saving into file (task 9) done.

--- week05/Form1.Designer.cs
+++ week05/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.exportProfitsButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -50,11 +51,21 @@
             this.dataGridView2.Size = new System.Drawing.Size(240, 150);
             this.dataGridView2.TabIndex = 1;
             // 
+            // exportProfitsButton
+            // 
+            this.exportProfitsButton.Location = new System.Drawing.Point(479, 13);
+            this.exportProfitsButton.Name = "exportProfitsButton";
+            this.exportProfitsButton.Size = new System.Drawing.Size(75, 23);
+            this.exportProfitsButton.TabIndex = 2;
+            this.exportProfitsButton.Text = "button1";
+            this.exportProfitsButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exportProfitsButton);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -69,6 +80,7 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button exportProfitsButton;
     }
 }
 
--- week05/Form1.cs
+++ week05/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,8 @@ namespace week05
 
             CreatePortfolio();
 
+            exportProfitsButton.Text = Resource1.ExportProfits;
+
             List<decimal> Nyereségek = new List<decimal>();
             int intervalum = 30;
             DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
@@ -45,6 +48,41 @@ namespace week05
                                       select x)
                                         .ToList();
             MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+
+            exportProfitsButton.Click += ExportProfitsButton_Click;
+        }
+
+        private void ExportProfitsButton_Click(object sender, EventArgs e)
+        {
+            //Azért nem szervezem ezt ki, mert egy feladat része volt, hogy pontosan így ott legyen a konstruktor végén a kód,
+            //ezért nem írom át azt a részt, hogy ugyanazt a funkciót töltse be, de jobb legyen az overall szempontból
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text File (*.txt) | *.txt";
+            dialog.DefaultExt = ".txt";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Időszak;Nyereség");
+
+                List<decimal> Nyereségek = new List<decimal>();
+                int intervalum = 30;
+                DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+                DateTime záróDátum = new DateTime(2016, 12, 30);
+                TimeSpan z = záróDátum - kezdőDátum;
+                for (int i = 0; i < z.Days - intervalum; i++)
+                {
+                    decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                               - GetPortfolioValue(kezdőDátum.AddDays(i));
+                    Nyereségek.Add(ny);
+                    sb.AppendLine(i + ";" + ny);
+                }
+
+                byte[] textInByte = new UTF8Encoding(true).GetBytes(sb.ToString());
+                Stream stream = dialog.OpenFile();
+                stream.Write(textInByte, 0, textInByte.Length);
+                stream.Flush();
+                stream.Close();
+            }
         }
 
         private void CreatePortfolio()
