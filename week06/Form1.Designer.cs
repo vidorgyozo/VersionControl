5d90e47c9de2e36e7e6ad06ad1e3a13c1c98e0d0
vidorgyozo <gyozovidor@gmail.com>
2020-12-05 17:38:12 +0100

    Task04 done.

--- week06/Form1.Designer.cs
+++ week06/Form1.Designer.cs
@@ -29,24 +29,36 @@
         private void InitializeComponent()
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ratesDataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ratesDataGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Location = new System.Drawing.Point(673, 333);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(754, 404);
+            this.richTextBox1.Size = new System.Drawing.Size(115, 105);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
+            // 
+            // ratesDataGrid
+            // 
+            this.ratesDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ratesDataGrid.Location = new System.Drawing.Point(13, 13);
+            this.ratesDataGrid.Name = "ratesDataGrid";
+            this.ratesDataGrid.Size = new System.Drawing.Size(775, 289);
+            this.ratesDataGrid.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ratesDataGrid);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ratesDataGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView ratesDataGrid;
     }
 }
 
--- week06/Form1.cs
+++ week06/Form1.cs
@@ -7,17 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.Entities;
 using week06.MnbServiceReference;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
 
             GetExchangeRates();
+
+            ratesDataGrid.DataSource = Rates;
+
+
         }
 
         private void GetExchangeRates()
