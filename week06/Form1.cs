09870eeb49376d383b9cdb3a225c9736e2cf1ca4
vidorgyozo <gyozovidor@gmail.com>
2020-12-05 17:25:11 +0100

    Task 3 done with a rich text box as means of checking the result.

--- week06/Form1.cs
+++ week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.MnbServiceReference;
 
 namespace week06
 {
@@ -15,6 +16,23 @@ namespace week06
         public Form1()
         {
             InitializeComponent();
+
+            GetExchangeRates();
+        }
+
+        private void GetExchangeRates()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+
+            GetExchangeRatesResponseBody response = mnbService.GetExchangeRates(request);
+
+            string result = response.GetExchangeRatesResult;
+
+            richTextBox1.Text = result;
         }
     }
 }
