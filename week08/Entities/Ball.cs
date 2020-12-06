3f83f3401c2a0172c40ed98e703f30bdc209f61e
vidorgyozo <gyozovidor@gmail.com>
2020-12-06 09:23:43 +0100

    task6 done

--- week08/Entities/Ball.cs
+++ week08/Entities/Ball.cs
@@ -5,32 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08.Abstractions;
 
 namespace week08.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Height = 50;
-            Width = 50;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics graphics)
+        protected override void DrawImage(Graphics graphics)
         {
             graphics.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-
-        public void MoveBall()
-        {
-            Left++;
         }
     }
 }
