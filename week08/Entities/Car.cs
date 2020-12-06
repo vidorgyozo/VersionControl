12b5ba5a0d8cccbd374b2b368eed67cf11a9b305
vidorgyozo <gyozovidor@gmail.com>
2020-12-06 09:28:50 +0100

    task7 done

---
+++ week08/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week08.Abstractions;
+
+namespace week08.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics graphics)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            graphics.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
