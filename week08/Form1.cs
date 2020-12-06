e8de17f19e9daf4529e3607934c9e6690b61f576
vidorgyozo <gyozovidor@gmail.com>
2020-12-06 09:16:14 +0100

    Task4 done.

--- week08/Form1.cs
+++ week08/Form1.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08.Entities;
 
 namespace week08
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            Ball newBall = Factory.CreateNew();
+            _balls.Add(newBall);
+            mainPanel.Controls.Add(newBall);
+            newBall.Left = -newBall.Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            int maxPos = 0;
+            foreach (Ball ball in _balls)
+            {
+                ball.MoveBall();
+                if (maxPos < ball.Left)
+                {
+                    maxPos = ball.Left;
+                }
+            }
+
+            if(maxPos > 1000)
+            {
+                Ball oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
