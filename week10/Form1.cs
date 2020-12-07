26fc09789e044f23275b43fa907043e7cb2cfe66
vidorgyozo <gyozovidor@gmail.com>
2020-12-07 22:03:58 +0100

    task 4 done

--- week10/Form1.cs
+++ week10/Form1.cs
@@ -17,6 +17,11 @@ namespace week10
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,8 +29,11 @@ namespace week10
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start(false);
         }
     }
 }
