e817b6fb7f33d6a3c749c8b1cd66d9a47f4c4746
vidorgyozo <gyozovidor@gmail.com>
2020-12-07 22:15:21 +0100

    task7 done

--- week10/Form1.cs
+++ week10/Form1.cs
@@ -22,6 +22,8 @@ namespace week10
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -45,6 +47,16 @@ namespace week10
 
         private void Gc_GameOver(object sender)
         {
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
             generation++;
             label1.Text = string.Format(
             "{0}. generáció",
